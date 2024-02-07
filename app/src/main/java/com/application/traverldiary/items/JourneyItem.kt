353c1e565514ce1b8b42edc1b32b1206639d36af
Shuaihao Chen <1785623097@qq.com>
2024-02-07 11:01:15 +0800

    修正：(fromTime-toTime)->(toTime-fromTime) -bug：TextView不显示

--- app/src/main/java/com/application/traverldiary/items/JourneyItem.kt
+++ app/src/main/java/com/application/traverldiary/items/JourneyItem.kt
@@ -5,5 +5,5 @@ import com.application.traverldiary.models.Journey
 open class JourneyItem(
     var journey: Journey,
     val topRatio: Float = (journey.fromTime) / 24f,
-    val heightRatio: Float = (journey.fromTime - journey.toTime) / 24f
+    val heightRatio: Float = (journey.toTime - journey.fromTime) / 24f
 )
