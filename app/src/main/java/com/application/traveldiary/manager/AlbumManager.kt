ff513330565847d8c63b3b7f0662b4e632c0f7f9
GuHeng <17596007559@163.com>
2024-02-14 13:32:55 +0800

    改进：LocationTimeMAnager完善 Location高德地图API

--- app/src/main/java/com/application/traveldiary/manager/AlbumManager.kt
+++ app/src/main/java/com/application/traveldiary/manager/AlbumManager.kt
@@ -22,7 +22,8 @@ import java.util.Locale
 
 
 class AlbumManager private constructor() {
-    private val fileManager = FileManager.getInstance(fileDir,)
+    private val fileManager = FileManager.getInstance(fileDir)
+    private val locationTimeManager = LocationTimeManager.getInstance()
     companion object {
         private var instance: AlbumManager? = null
         //保存的照片路径
@@ -69,67 +70,17 @@ class AlbumManager private constructor() {
         val inputStream: InputStream? = context.contentResolver.openInputStream(uri)
         val exifInterface = inputStream?.let { ExifInterface(it) }
 
-        val picDate = getPhotoTakeTime(exifInterface)
-
-        val latLong = FloatArray(2)
-        val hasLatLong = exifInterface?.getLatLong(latLong)
-        var picAddress:String = ""
-//        if (hasLatLong == true) {
-//            //创建一个新的Geocoder实例
-//            val geocoder = Geocoder(context, Locale.getDefault())
-//            // 创建一个新的Location实例
-//            val location: Location = Location("") // 你的位置
-//            location.latitude = latLong[0].toDouble()
-//            location.latitude = latLong[1].toDouble()
-//            // 获取地址列表
-//            Log.v("wq","${location.latitude}  ${location.longitude}")
-//            val addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1)
-//            // 获取第一个地址
-//            val address = addresses?.get(0)
-//            if (address != null) {
-//                picAddress = address.getAddressLine(0)
-//            }
-//        }
+        //照片拍摄时间
+        val picDate = locationTimeManager.getPhotoTakeTime(exifInterface)
+        //获取照片拍摄地点
+        val picAddress = ""
+        //获取保存在应用内部的照片Uri
         val mUri = fileManager.savePicFile(uri,picDate)
 
         return Picture(mUri,picDate,picAddress)
     }
 
-    //获取照片位置
-    fun getPhotoLocationFromUri(context: Context, uri: Uri,exifInterface: ExifInterface?) {
-        val latitude = exifInterface?.getAttribute(ExifInterface.TAG_GPS_LATITUDE)
-        val longitude = exifInterface?.getAttribute(ExifInterface.TAG_GPS_LONGITUDE)
-        if (latitude != null && longitude != null) {
-            convertExifToDecimal(latitude)
-            convertExifToDecimal(longitude)
-        } else {
-            // 照片没有位置信息，进行适当的处理
-        }
-    }
-    //经纬度 转为标准的数字格式
-    private fun convertExifToDecimal(exifCoord: String): Double {
-        val split = exifCoord.split(",")
-
-        val degrees = split[0].split("/").let { it[0].toDouble() / it[1].toDouble() }
-        val minutes = split[1].split("/").let { it[0].toDouble() / it[1].toDouble() }
-        val seconds = split[2].split("/").let { it[0].toDouble() / it[1].toDouble() }
-
-        return degrees + minutes / 60.0 + seconds / 3600.0
-    }
-
-    //获取照片拍摄时间
-    private fun getPhotoTakeTime(exifInterface: ExifInterface?):String{
-        val dateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
-        val dateTimeString = exifInterface?.getAttribute(ExifInterface.TAG_DATETIME_ORIGINAL)
-        val format = SimpleDateFormat("yyyy:MM:dd HH:mm:ss", Locale.getDefault())
-        val takeDate = dateTimeString?.let { format.parse(it) }
-        return if (takeDate==null){
-                dateFormat.format(Date())
-            }else{
-                dateFormat.format(takeDate)
-            }
-    }
-
+    //标准化list
     private fun standardizeList(list:List<Picture>):MutableList<Any>{
         //穿插进去时间地点
         val resultList = mutableListOf<Any>()
---
+++ app/src/main/java/com/application/traveldiary/manager/LocationTimeManager.kt
@@ -0,0 +1,62 @@
+package com.application.traveldiary.manager
+
+import android.content.Context
+import android.media.ExifInterface
+import android.net.Uri
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class LocationTimeManager private constructor() {
+    companion object {
+        private var instance: LocationTimeManager? = null
+
+        //经典单例模式
+        fun getInstance(): LocationTimeManager {
+            if (instance == null) {
+                synchronized(this) {
+                    if (instance == null) {
+                        instance = LocationTimeManager()
+                    }
+                }
+            }
+            return instance!!
+        }
+    }
+
+    //获取照片位置
+    fun getPhotoLocationFromUri(context: Context, uri: Uri, exifInterface: ExifInterface?) {
+        val latitude = exifInterface?.getAttribute(ExifInterface.TAG_GPS_LATITUDE)
+        val longitude = exifInterface?.getAttribute(ExifInterface.TAG_GPS_LONGITUDE)
+        if (latitude != null && longitude != null) {
+            convertExifToDecimal(latitude)
+            convertExifToDecimal(longitude)
+        } else {
+            // 照片没有位置信息，进行适当的处理
+        }
+    }
+    //经纬度 转为标准的数字格式
+    private fun convertExifToDecimal(exifCoord: String): Double {
+        val split = exifCoord.split(",")
+
+        val degrees = split[0].split("/").let { it[0].toDouble() / it[1].toDouble() }
+        val minutes = split[1].split("/").let { it[0].toDouble() / it[1].toDouble() }
+        val seconds = split[2].split("/").let { it[0].toDouble() / it[1].toDouble() }
+
+        return degrees + minutes / 60.0 + seconds / 3600.0
+    }
+
+    //获取照片拍摄时间
+    fun getPhotoTakeTime(exifInterface: ExifInterface?):String{
+        val dateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
+        val dateTimeString = exifInterface?.getAttribute(ExifInterface.TAG_DATETIME_ORIGINAL)
+        val format = SimpleDateFormat("yyyy:MM:dd HH:mm:ss", Locale.getDefault())
+        val takeDate = dateTimeString?.let { format.parse(it) }
+        return if (takeDate==null){
+            dateFormat.format(Date())
+        }else{
+            dateFormat.format(takeDate)
+        }
+    }
+
+}
